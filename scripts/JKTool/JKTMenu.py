31471ddfb1477da2050e12da4accdd1d40904ff7
dundeegames <jiri.klic@dundeegames.com>
2015-12-14 23:17:27 +0000

    Shelf icon and gitignore

--- scripts/JKTool/JKTMenu.py
+++ scripts/JKTool/JKTMenu.py
@@ -1,10 +1,15 @@
 import maya.cmds as mc
 import maya.mel as mel
 
+
 def createMyMenu():
 	gMainWindow = mel.eval('$tmpVar=$gMainWindow')
 	mc.menu("JKTool_UI", label="JKTool", tearOff=True, parent=gMainWindow )
-	
+	mc.menuItem( label="Run", command="mc.JKTRun()" )
+	mc.menuItem( label='Update', enable=False )
+	# mc.menuItem( label='Save' )
+	mc.menuItem( divider=True )
+	mc.menuItem( label='Help', enable=False )
 
 def deleteMyMenu():
 	mc.deleteUI("JKTool_UI")
